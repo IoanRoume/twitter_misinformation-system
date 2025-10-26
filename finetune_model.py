e60348c414d1ba86dd753b79758ea9f3b61891de
IOANNIS ROUMELIOTIS <it2021087@hua.gr>
2025-10-26 21:29:38 +0200

    Reverted settings for test

--- finetune_model.py
+++ finetune_model.py
@@ -33,10 +33,6 @@ model = AutoModelForSequenceClassification.from_pretrained(MODEL_NAME, num_label
 model.config.id2label = id2label
 model.config.label2id = label2id
 
-# Use only 10% of data for quick test
-ds['train'] = ds['train'].select(range(len(ds['train']) // 10))
-ds['test'] = ds['test'].select(range(len(ds['test']) // 10))
-print("Tokenizing dataset...")
 tokenized_train = ds['train'].map(tokenize_function, batched=True)
 tokenized_test = ds['test'].map(tokenize_function, batched=True)
 
@@ -47,7 +43,7 @@ training_args = TrainingArguments(
     learning_rate=2e-05,              
     per_device_train_batch_size=16,
     per_device_eval_batch_size=16,
-    num_train_epochs=1,              
+    num_train_epochs=3,              
     weight_decay=0.01,               
     save_total_limit=1,              
     load_best_model_at_end=True,     
